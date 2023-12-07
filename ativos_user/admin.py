34a2f3187ef56d7b81bdb567764db3d68eedd3af
JB Silva <jbsilva.dev@outlook.com>
2023-12-07 12:08:48 -0300

    confg inicial favoritos user

--- ativos_user/admin.py
+++ ativos_user/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from ativos_user.models import AtivosUser, CodigoAtivo
+
+class ListaAtivosUser(admin.ModelAdmin):
+  list_display = ('id', 'codigo_ativo', 'nome_empresa')
+  list_display_links = ('codigo_ativo',)
+  list_filter = ('codigo_ativo',)
+  list_per_page = 10
+
+
+class ListaCodigosAtivoUser(admin.ModelAdmin):
+  list_display = ('id', 'codigo', 'favorito', 'em_carteira', 'variacao_percent')
+  list_display_links = ('codigo',)
+  list_filter = ('codigo',)
+  list_per_page = 10
+
+admin.site.register(AtivosUser, ListaAtivosUser)
+admin.site.register(CodigoAtivo, ListaCodigosAtivoUser)
--- ativos_user/views.py
+++ ativos_user/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from ativos_user.models import AtivosUser, CodigoAtivo
+
 # Create your views here.
 def favoritos(request):
-  return render(request, 'favoritos.html')
+  busca = request.GET.get('cod-ativo')
+  if busca:
+    ativos = CodigoAtivo.objects.filter(codigo__icontains = busca)
+  else:
+    ativos = CodigoAtivo.objects.all()
+  return render(request, 'favoritos.html', {'ativos':ativos})
