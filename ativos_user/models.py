d1afae6df863be7ff8127b0da2d3ddb4038398b0
JB Silva <jbsilva.dev@outlook.com>
2023-12-07 11:18:51 -0300

    tabela auxiliar para codigo dos ativos por usuario

--- ativos_user/models.py
+++ ativos_user/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class CodigoAtivo(models.Model):
     codigo = models.CharField(max_length=5, )
     favorito = models.BooleanField()
+    em_carteira = models.BooleanField()
 
     def __str__(self):
         return self.codigo
@@ -14,7 +15,6 @@ class AtivosUser(models.Model):
     user_id = models.IntegerField()
     nome_empresa = models.CharField(max_length=100)
     codigo_ativo = models.ForeignKey('CodigoAtivo', related_name = 'cod_ativos', on_delete=models.CASCADE)
-    em_carteira = models.BooleanField()
 
     def __str__(self):
         return self.nome_empresa
