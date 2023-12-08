ec5c0bd6ee501171c5db0d9271c9e29a53a87ddb
JB Silva <jbsilva.dev@outlook.com>
2023-12-08 08:55:02 -0300

    pouxando id de usuario logada para exibir apenas favoritos

--- ativos_global/models.py
+++ ativos_global/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class AtivosList(models.Model):
-  cod_ativo = models.CharField(max_length=4)
+  cod_ativo = models.CharField(max_length=5)
   empresa_nome = models.CharField(max_length=100)
 
   def __str__(self):
--- ativos_user/views.py
+++ ativos_user/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 from ativos_user.models import AtivosUser
 
 # Create your views here.
 def favoritos(request):
-  user_id = 1 # Puxar ID do usuario aqui
+  user_id = request.user.id # ID do usuario logado
   busca = request.GET.get('cod-ativo')
   if busca:
     ativos = AtivosUser.objects.filter(codigo_ativo__icontains = busca).filter(user_id = user_id)
