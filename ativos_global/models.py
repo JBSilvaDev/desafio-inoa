b004da9db45ea898f9f414f6d1a1e260f6c28368
JB Silva <jbsilva.dev@outlook.com>
2023-12-15 18:39:44 -0300

    testando extração de lista de ativos do site da b3

--- ativos_global/models.py
+++ ativos_global/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class AtivosList(models.Model):
-  cod_ativo = models.CharField(max_length=5)
+  cod_ativo = models.CharField(max_length=6)
   nome_empresa = models.CharField(max_length=100)
 
   def __str__(self):
--- ativos_user/models.py
+++ ativos_user/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class AtivosUser(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     id_ativo_list = models.IntegerField()
-    cod_ativo = models.CharField(max_length=5, )
+    cod_ativo = models.CharField(max_length=6, )
     nome_empresa = models.CharField(max_length=100)
     favorito = models.BooleanField()
     em_carteira = models.BooleanField()
