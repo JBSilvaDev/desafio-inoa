64f1658dae2f5f7b3e7fe04b3cb0a47b7f75e68b
JB Silva <jbsilva.dev@outlook.com>
2023-12-17 17:42:24 -0300

    ajustes para acesso aos favoritos e exibicao de botoes conforme login

--- ativos_user/views.py
+++ ativos_user/views.py
@@ -2,13 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from ativos_user.models import AtivosUser
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url='login')
 def favoritos(request):
   user_id = request.user.id # ID do usuario logado
-  if not user_id:
-    return HttpResponse("Voce precisa esta logado")
   busca = request.GET.get('cod-ativo')
   if busca:
     ativos = AtivosUser.objects.filter(cod_ativo__icontains = busca).filter(user_id = user_id)
