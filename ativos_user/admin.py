80d077b7d8a384f34f9aee95e014a3883ddcba22
JB Silva <jbsilva.dev@outlook.com>
2026-01-08 09:37:22 -0300

    ajustes para funcionamento

--- ativos_user/admin.py
+++ ativos_user/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
+from .models import AtivosUser, PrecoAtivo
 
-from ativos_user.models import AtivosUser
+class AtivosUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ativo', 'limite_superior', 'limite_inferior', 'intervalo_verificacao')
+    list_display_links = ('user', 'ativo')
+    search_fields = ('user__username', 'ativo__cod_ativo')
+    list_per_page = 10
 
-class ListaAtivosUser(admin.ModelAdmin):
-  list_display = ('user_id', 'cod_ativo','nome_empresa','favorito', 'em_carteira', 'variacao_percent')
-  list_display_links = ('cod_ativo','nome_empresa')
-  search_fields = ('cod_ativo','nome_empresa')
-  list_per_page = 10
+class PrecoAtivoAdmin(admin.ModelAdmin):
+    list_display = ('ativo', 'preco', 'data_hora')
+    list_display_links = ('ativo',)
+    search_fields = ('ativo__cod_ativo',)
+    list_per_page = 10
 
-
-
-admin.site.register(AtivosUser, ListaAtivosUser)
+admin.site.register(AtivosUser, AtivosUserAdmin)
+admin.site.register(PrecoAtivo, PrecoAtivoAdmin)
--- ativos_user/models.py
+++ ativos_user/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ativos_global.models import AtivosList
 
-
-   
 
 class AtivosUser(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_ativo_list = models.IntegerField()
-    cod_ativo = models.CharField(max_length=6, )
-    nome_empresa = models.CharField(max_length=100)
-    favorito = models.BooleanField()
-    em_carteira = models.BooleanField()
-    variacao_percent = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ativo = models.ForeignKey(AtivosList, on_delete=models.CASCADE)
+    limite_superior = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    limite_inferior = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    intervalo_verificacao = models.IntegerField(default=60)  # in minutes
 
     def __str__(self):
-        return self.nome_empresa
+        return f'{self.user.username} - {self.ativo.cod_ativo}'
+
+class PrecoAtivo(models.Model):
+    ativo = models.ForeignKey(AtivosList, on_delete=models.CASCADE)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    data_hora = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.ativo.cod_ativo} - {self.preco} - {self.data_hora}'
